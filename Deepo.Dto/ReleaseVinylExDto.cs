813b325c185f3f76fc0323ea6244e19470bf94cf
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2025-06-23 10:21:45 +0200

    Add country localization and update vinyl detail layout

--- Deepo.Dto/ReleaseVinylExDto.cs
+++ Deepo.Dto/ReleaseVinylExDto.cs
@@ -7,5 +7,6 @@ public class ReleaseVinylExDto : ReleaseVinylDto
 {
     public string Label { get; set; } = string.Empty;
     public string Country { get; set; } = string.Empty;
+    public string? Duration { get; set; } = string.Empty;
     public Dictionary<string, string> Genres { get; } = [];
 }
