c2b5da0c9ae7cd347d6f35be4144b27c8bef777e
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2025-07-05 01:23:44 +0200

    Modify album track tables and update utility script

--- Deepo.DAL.SQL/Script/Script.Utils.sql
+++ Deepo.DAL.SQL/Script/Script.Utils.sql
@@ -1,11 +1,82 @@
 ﻿
-delete from Genre_Album_Release
-delete from Genre_Album
-delete from Asset_Release
-delete from Asset
-delete from Author_Release
-delete from Author
-delete from Tracklist_Album
-delete from Provider_Release
-delete from Release_Album
-delete from Release
+-- ##### Delete Release by GUID ##### --
+BEGIN TRANSACTION;
+
+DECLARE @TargetGUID NVARCHAR(68) = '99273e62-ab24-4918-b60a-8e0059b0d866';
+DECLARE @ReleaseID INT;
+DECLARE @ReleaseAlbumID INT;
+
+SELECT @ReleaseID = [Release_ID]
+FROM [dbo].[Release]
+WHERE [GUID] = @TargetGUID;
+
+IF @ReleaseID IS NULL
+BEGIN
+    PRINT 'No Release found with GUID: ' + @TargetGUID;
+    ROLLBACK TRANSACTION;
+    RETURN;
+END
+
+SELECT @ReleaseAlbumID = [Release_Album_ID]
+FROM [dbo].[Release_Album]
+WHERE [Release_ID] = @ReleaseID;
+
+IF @ReleaseAlbumID IS NOT NULL
+BEGIN
+    DELETE FROM [dbo].[Tracklist_Album]
+    WHERE [Release_Album_ID] = @ReleaseAlbumID;
+END
+
+IF @ReleaseAlbumID IS NOT NULL AND EXISTS (SELECT 1 FROM sys.tables WHERE name = 'Genre_Album_Release')
+BEGIN
+    DELETE FROM [dbo].[Genre_Album_Release]
+    WHERE [Release_Album_ID] = @ReleaseAlbumID;
+END
+
+IF @ReleaseAlbumID IS NOT NULL
+BEGIN
+    DELETE FROM [dbo].[Release_Album]
+    WHERE [Release_Album_ID] = @ReleaseAlbumID;
+END
+
+DELETE FROM [dbo].[Author_Release]
+WHERE [Release_ID] = @ReleaseID;
+
+DELETE FROM [dbo].[Asset_Release]
+WHERE [Release_ID] = @ReleaseID;
+
+DELETE FROM [dbo].[Provider_Release]
+WHERE [Release_ID] = @ReleaseID;
+
+DELETE FROM [dbo].[Availability_Release]
+WHERE [Release_ID] = @ReleaseID;
+
+DELETE FROM [dbo].[Release_Movie]
+WHERE [Release_ID] = @ReleaseID;
+
+DELETE FROM [dbo].[Release_TVShow]
+WHERE [Release_ID] = @ReleaseID;
+
+DELETE FROM [dbo].[Release]
+WHERE [Release_ID] = @ReleaseID;
+
+ROLLBACK TRANSACTION;
+
+-------------------------------------------------------------
+
+-- ##### Delete All Releases #####
+
+BEGIN TRANSACTION;
+
+DELETE FROM Genre_Album_Release
+DELETE FROM Genre_Album
+DELETE FROM Asset_Release
+DELETE FROM Asset
+DELETE FROM Author_Release
+DELETE FROM Author
+DELETE FROM Tracklist_Album
+DELETE FROM Provider_Release
+DELETE FROM Release_Album
+DELETE FROM Release
+
+ROLLBACK TRANSACTION;
--- Deepo.DAL.SQL/dbo/Tables/Tracklist_Album.sql
+++ Deepo.DAL.SQL/dbo/Tables/Tracklist_Album.sql
@@ -1,7 +1,8 @@
-﻿CREATE TABLE [dbo].[Track_Album](
-	[Track_Album_ID] INT IDENTITY (1,1) NOT NULL, 
-    [Position]           INT                NOT NULL, 
-    [Title]              NVARCHAR(1000)     NOT NULL, 
-    [Duration]           NVARCHAR(50)       NOT NULL,
-    CONSTRAINT [PK_Track_Album] PRIMARY KEY CLUSTERED ([Track_Album_ID] ASC)
+﻿CREATE TABLE [dbo].[Tracklist_Album](
+	[Tracklist_Album_ID] INT IDENTITY (1, 1) NOT NULL,
+	[Release_Album_ID]	 INT				 NOT NULL,
+	[Track_Album_ID]	 INT				 NOT NULL,
+	CONSTRAINT [PK_Tracklist_Album] PRIMARY KEY CLUSTERED ([Tracklist_Album_ID] ASC),
+	CONSTRAINT [FK_Tracklist_Album_Release_Album] FOREIGN KEY ([Release_Album_ID]) REFERENCES [dbo].[Release_Album] ([Release_Album_ID]),
+	CONSTRAINT [FK_Tracklist_Album_Track_Album] FOREIGN KEY ([Track_Album_ID]) REFERENCES [dbo].[Track_Album] ([Track_Album_ID])
 )
